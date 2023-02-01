91f2a6ad3012fae8558ed07ca1d64cce6f157174
Andrea Alvarez <andreaelenaalvarez2019@gmail.com>
2023-02-01 09:49:33 -0300

    creo stock

---
+++ stock.js
@@ -0,0 +1,1 @@
+let stock=[{nombre:'producto 1',imagen:'img/1559679036_977776_1559679371_noticia_normal.jpg',precio:10000,id:1},{nombre:'producto 2',imagen:'img/descarga.jpg',precio:8000,id:2},{nombre:'producto 3',imagen:'img/1559679036_977776_1559679371_noticia_normal.jpg',precio:10000,id:3},]
